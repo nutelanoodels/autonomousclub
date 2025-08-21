272686bd57ca37c2f2090a455d1e4c91df8e0d9c
sevpuri <168060898+nutelanoodels@users.noreply.github.com>
2025-08-21 18:44:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.WAVES({
+    el: "#tsparticles",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    shininess: 21.00,
+    waveHeight: 34.50,
+    waveSpeed: 0.40,
+    zoom: 0.65
+  });
+});
